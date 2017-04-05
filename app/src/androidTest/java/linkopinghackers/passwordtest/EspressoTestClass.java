95c90d697cfb68846aa764302dc1c813489bcaf0
minisemi <a.ulander@live.se>
2017-04-05 23:35:26 +0200

    Import junit

--- app/src/androidTest/java/linkopinghackers/passwordtest/EspressoTestClass.java
+++ app/src/androidTest/java/linkopinghackers/passwordtest/EspressoTestClass.java
@@ -1,10 +1,8 @@
 package linkopinghackers.passwordtest;
 
-import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import org.hamcrest.BaseMatcher;
@@ -13,14 +11,12 @@ import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withTagKey;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
